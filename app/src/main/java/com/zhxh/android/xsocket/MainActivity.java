85aee2a6f57f41e6692bb476f3d6e1cb9fffbef2
zhxh <zhxhcoder@gmail.com>
2018-06-30 17:50:39 +0800

    socket

--- app/src/main/java/com/zhxh/android/xsocket/MainActivity.java
+++ app/src/main/java/com/zhxh/android/xsocket/MainActivity.java
@@ -1,13 +1,69 @@
 package com.zhxh.android.xsocket;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button btnSend;
+    private static final String HOST = "222.17.106.37";
+    private static final int PORT = 5252;
+    private Socket socket;
+    private PrintWriter out;
+    // private BufferedReader in;
+    private String getText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        btnSend = findViewById(R.id.btnSend);
+        //开启新线程访问网络，否则会报错
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    socket = new Socket(HOST, PORT);
+                    // in = new BufferedReader(new InputStreamReader(
+                    // socket.getInputStream()));
+                    out = new PrintWriter(new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream(),
+                                    "utf-8")), true);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        btnSend.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                getText = editText.getText().toString();
+                if (socket.isConnected()) {
+                    if (!socket.isOutputShutdown()) {
+                        out.println(getText);
+                    }
+                }
+            }
+        });
     }
+
 }
--- xsocketlib/src/main/java/com/zhxh/xsocketlib/SocketServer.java
+++ xsocketlib/src/main/java/com/zhxh/xsocketlib/SocketServer.java
@@ -1,7 +1,47 @@
 package com.zhxh.xsocketlib;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by zhxh on 2018/6/30
  */
 public class SocketServer {
+    // 端口号
+    private static final int PORT = 5252;
+    // ServerSocket类
+    private ServerSocket mServerSocket;
+    private Socket mSocket;
+    private BufferedReader in;
+
+    // private PrintWriter out;
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        new SocketServer();
+    }
+
+    public SocketServer() {
+        super();
+        // TODO Auto-generated constructor stub
+        try {
+            mServerSocket = new ServerSocket(PORT);
+            System.out.println("------Server Satrted------");
+            mSocket = mServerSocket.accept();
+            in = new BufferedReader(new InputStreamReader(
+                    mSocket.getInputStream()));
+            // out = new PrintWriter(mSocket.getOutputStream(), true);
+            String line = in.readLine();
+            System.out.println("you sent msg is " + line);
+            // out.close();
+            in.close();
+            mSocket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 }
