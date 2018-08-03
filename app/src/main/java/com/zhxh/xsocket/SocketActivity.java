2ecafdc9a1fea8db3d542b46cc30f96fda8e9e6f
zhxh <zhxhcoder@gmail.com>
2018-08-03 17:19:10 +0800

    OnDataReceivedListener

--- app/src/main/java/com/zhxh/xsocket/SocketActivity.java
+++ app/src/main/java/com/zhxh/xsocket/SocketActivity.java
@@ -3,10 +3,19 @@ package com.zhxh.xsocket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhxh.xsocketlib.socket.AsyncSocketService;
 import com.zhxh.xsocketlib.socket.OnDataReceivedListener;
+import com.zhxh.xsocketlib.socket.SocketData;
+
+import java.util.ArrayList;
 
 public class SocketActivity extends AppCompatActivity {
+
+    AsyncSocketService socket;
+
     OnDataReceivedListener socketListener;
+
+    ArrayList<SocketData> stockListSocket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,32 @@ public class SocketActivity extends AppCompatActivity {
         setContentView(R.layout.activity_socket);
 
 
-        socketListener.onConnected();
+        socketListener = new OnDataReceivedListener() {
+            @Override
+            public void onReceiveData(String data) {
+
+            }
+
+            @Override
+            public void onConnected() {
+
+            }
+
+            @Override
+            public void onDisconnected() {
+
+            }
+        };
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
